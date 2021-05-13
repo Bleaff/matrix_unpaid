05bc6ffeecac46a43cfe72c5ba2bced199a0adc0
Serjo750 <59150974+Serjo750@users.noreply.github.com>
2021-05-13 20:06:39 +0300

    hfd

---
+++ Matrix/test.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include<vector>	
+
+using namespace std;
+
+typedef struct _Matrix {
+	int col;
+	int row;
+	vector < vector<int> matrix;
+};
+
+
+class Matrix {
+private:
+
+	int col;
+	int row;
+	vector<vector<int>> matrix;
+
+public:
+	Matrix()
+	{
+
+	}
+	~Matrix()
+	{
+
+	}
+
+};
